4aead713552aef682d477ac312fefbd4a6b12584
coca-paiz <117420913+coca-paiz@users.noreply.github.com>
2022-11-03 21:06:39 -0600

    Prueba pull jeje

--- pruebaPull.js
+++ pruebaPull.js
@@ -1,3 +1,4 @@
-console.log("prueba del pull de git")
+console.log("prueba de clone")
 //Ahora estoy agregando un comentario, simplemente para ver la funcionalidad del comando pull. hehe. 
-//Ahora estoy probando la funcionalidad de fetch, hehe. 
+//Ahora estoy probando la funcionalidad de fetch, hehe y por ende merge. 
+//Ahora estoy haciendo una prueba de pull
