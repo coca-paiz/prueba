069994314ea4042ef150423eb922c7f4bd9a07ec
coca-paiz <117420913+coca-paiz@users.noreply.github.com>
2022-11-03 21:03:57 -0600

    probando fetch

--- pruebaPull.js
+++ pruebaPull.js
@@ -1,2 +1,3 @@
 console.log("prueba del pull de git")
 //Ahora estoy agregando un comentario, simplemente para ver la funcionalidad del comando pull. hehe. 
+//Ahora estoy probando la funcionalidad de fetch, hehe. 
